f3cd74ffa4146dcc72ac4eef7c09b88b0fec64c6
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-05 01:17:29 +0000

    implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require("./eqArrays");
+
+
+const assertArraysEqual = function(array1, array2) {
+  result = eqArrays(array1, array2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Asseration Failed: [${array1}] !== [${array2}]`);
+  }
+};
