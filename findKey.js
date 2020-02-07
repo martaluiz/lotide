598327f7f77a0b50ba4fb2a3a98ad2e5f4bf32de
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-07 00:30:15 +0000

    Implement findKey

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+// TEST CASES
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri"); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "Blue Hill"); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0), undefined); // => "noma"
