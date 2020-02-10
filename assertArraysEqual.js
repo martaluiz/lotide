c4f2727a22c1eab8c8672e2a76a53160d3409cb7
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-10 23:28:26 +0000

    file test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,20 @@
-const assertArrayEqual = function (array1, array2) {
-  //console.log(array1)
-  if (array1.length !== array2.length) {
-    console.log(`🔴🔴🔴 Assertion Failed:  ${array1} !== ${array2}`);
-    return false;
-  }
+const assertEqual = require('./eqArrays');
 
-  for (let i = 0; i < array1.length; i++) {
+const assertArrayEqual = function (array1, array2) {
+   //console.log(array1)
+   if (array1.length !== array2.length) {
+     console.log(`🔴🔴🔴 Assertion Failed:  ${array1} !== ${array2}`);
+     return false;
+   }
+  };
     if (array1[i] !== array2[i]) {
       console.log(`🔴🔴🔴 Assertion Failed:  ${array1} !== ${array2}`);
       return false
     }
-  }
-
+  
   console.log(`🟢🟢🟢 Assertion Passed:  ${array1} ==== ${array2}`);
 
   return true;
-};
+
+
+module.exports = assertArraysEqual;
