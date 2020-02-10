6be040ceb8ee02ff9f3864393f0d324627c0211d
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-10 22:53:30 +0000

    test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed:  ${actual} ==== ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed:  ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2){
  if(array1.length !== array2.length){
@@ -19,7 +11,8 @@ const eqArrays = function(array1, array2){
      }
    }
    return true;
+   
  }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
